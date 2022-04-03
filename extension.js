39372a5c58294ae42c2a3a20e36117b6d1bf5a7d
vipin.v <vipin.v@infospica.com>
2022-04-03 14:15:41 +0530

    initial config

--- extension.js
+++ extension.js
@@ -1,27 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
+const { TextEncoder } = require('util');
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	
 	console.log('Congratulations, your extension "my" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('my.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('my.helloWorld',async function () {
+		const params = [
+			{ label : 'action',details : 'Action Folder'},
+			{ label : 'Reducers',details : 'Reducers Folder'},
+			{ label : 'Screen',details : 'screen Folder'},
+			{ label : 'Component',details : 'component Folder'}
+		]
 		// The code you place here will be executed every time your command is executed
+		// let typeOfFile = await vscode.window.showQuickPick(params,{
+		// 	matchOnDetail : true
+		// })
+		const rootProjectPath = vscode.workspace.rootPath
+
+
+
+		let { document } = vscode.window.activeTextEditor;
+		const currentFilePath = document.uri.path
+		const currentActiveFileData = document.uri.scheme
+		await vscode.workspace.fs.writeFile(vscode.Uri.file(`${rootProjectPath}/action/index.js`),new TextEncoder().encode('dsdsdsd '))
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from My!');
+		// vscode.window.showInformationMessage('Hello World from My!'+typeOfFile.label);
 	});
 
 	context.subscriptions.push(disposable);
